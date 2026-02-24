83c42afd7eb1dac287b4b3e54fdd4836019e7897
Femas Akbar Faturrohim <femasakbar69@gmail.com>
2026-02-24 16:53:14 +0700

    P3: Praktikum 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        return 'NIM : 244107020200<br>Nama : Fema Akbar Fathurohim<br>';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,35 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PhotoController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::resource('photos', PhotoController::class);
+
+Route::get('/', [HomeController::class,'index']);
+
+Route::get('/hello', [WelcomeController::class,'hello']);
+
+Route::get('/world', function () {
+    return 'World';
+}); 
+
+
+Route::get('/about', [AboutController::class,'about']);
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name; 
+});
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', [ArticleController::class,'articles']);
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
 });
